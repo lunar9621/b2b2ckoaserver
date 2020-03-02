740c947609b8a257f6e24bf321ea8c182813770e
lunar9621 <546604583@qq.com>
2020-03-02 16:37:36 +0800

    final

--- mongodb.js
+++ mongodb.js
@@ -8,13 +8,6 @@ MongoClient.connect(dbUrl,function(err,client){
         return;
     }
     var db=client.db(dbName);
-    //增加数据
-    // db.collection('todos').insertOne({'index':1,'name':'烘干','done':false},function(err,result){
-    //     if(!err){
-    //        console.log('增加数据成功');
-    //        client.close();
-    //     }
-    // })
     //查询数据
     var result=db.collection('todos').find({});
     result.toArray((err,docs)=>{
